2d25bdb2cd9ea729977939b15e169c882f0b91df
igorrodriguessss <igorgui3103@gmail.com>
2024-06-13 18:13:14 -0300

    album

--- album.py
+++ album.py
@@ -1,3 +1,8 @@
-class album :
-    def __init__(self,titulo,ano,lista):
-        self.titulo = 
+class Album:
+    def __init__(self, titulo, ano, lista):
+        self.titulo = titulo
+        self.ano = ano
+        self.lista = lista
+
+    def mostrar_info(self):
+        print(f'titulo: {self.titulo} \nano: {self.ano}\nlista: {self.lista}')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from banda import Banda
 from musico import Musico
 from instrumento import Guitarra, Bateria
+from album import Album
 
 # Criando instrumentos
 guitarra = Guitarra('Fender Stratocaster', 'Cordas', 6)
@@ -15,5 +16,15 @@ banda = Banda('The Beatles')
 banda.adicionar_musico(musico1)
 banda.adicionar_musico(musico2)
 
+# Criando álbum
+lista_de_musicas = ("\nMusica1\nMusica2 \nMusica3 ")  # Adicione as músicas aqui
+album = Album('Álbum Teste', 2024, lista_de_musicas)
+
+# Adicionando o álbum à banda
+
+
 # Exibindo informações da banda
 banda.mostrar_musicos()
+
+# Exibindo informações do álbum
+album.mostrar_info()
